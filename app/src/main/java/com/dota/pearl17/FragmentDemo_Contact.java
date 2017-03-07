16807b0f8791074a5b9f9bd713cbf1aa2bfcc81d
shivikaNagpal <sheebu1997@gmail.com>
2017-03-07 15:27:03 +0530

    Update FragmentDemo_Contact.java

--- app/src/main/java/com/dota/pearl17/FragmentDemo_Contact.java
+++ app/src/main/java/com/dota/pearl17/FragmentDemo_Contact.java
@@ -17,6 +17,6 @@ public class FragmentDemo extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragmentdemo, container, false);
+        return inflater.inflate(R.layout.fragmentdemo_contactpage, container, false);
     }
 }
