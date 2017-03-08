fe8c3836279b7e11b936b4b0a42cb11e1d1006a4
SHIVIKA NAGPAL <sheebu1997@gmail.com>
2017-03-08 20:05:38 +0530

    About Us, Reach Us - Done.

--- app/src/main/java/com/dota/pearl17/AboutUsActivity.java
+++ app/src/main/java/com/dota/pearl17/AboutUsActivity.java
@@ -2,6 +2,8 @@ package com.dota.pearl17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Typeface;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        TextView tx = (TextView)findViewById(R.id.about_us_textView);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/goodpro_condmedium.otf");
+        tx.setTypeface(custom_font);
+
     }
 }
