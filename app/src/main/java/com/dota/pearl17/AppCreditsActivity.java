b91d27d072a54ad333ef7d64632721c52ff8b93e
watson57 <kaushikn97@gmail.com>
2017-03-09 12:54:05 +0530

    App Credits done... sort off

---
+++ app/src/main/java/com/dota/pearl17/AppCreditsActivity.java
@@ -0,0 +1,32 @@
+package com.dota.pearl17;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.net.Uri;
+
+public class AppCreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_credits);
+
+    }
+
+    public void openBrowser(View v){
+
+        String url=(String)v.getTag();
+
+        Intent i=new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        i.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        i.setData(Uri.parse(url));
+
+        startActivity(i);
+
+    }
+}
